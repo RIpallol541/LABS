5747a6dfcf1c8d373b2e7905168f9bf33409e414
RIpallol541 <ripallol.541@yandex.ru>
2023-05-13 10:52:10 +0300

    1
    1

--- Лаб11/laBAba11/laBAba11/IDADD.cs
+++ Лаб11/laBAba11/laBAba11/IDADD.cs
@@ -74,5 +74,34 @@ namespace laBAba11
             dataGridView1.DataSource = ds.Tables[0];
         }
 
-    }
+		private void button3_Click(object sender, EventArgs e)
+		{
+			SqlConnection Con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\ripal\\source\\repos\\laBAba11\\laBAba11\\Mem.mdf;Integrated Security=True");
+			Con.Open();
+			SqlCommand cmd = new SqlCommand("UPDATE Memor SET Достопремечательность = " + textBox1.Text + "WHERE ID =" + textBox7.Text);
+			SqlCommand cmd1 = new SqlCommand("UPDATE Memor SET Фото = " + textBox1.Text + "WHERE ID =" + textBox7.Text);
+			SqlCommand cmd2 = new SqlCommand("UPDATE Memor SET Открытие = " + textBox1.Text + "WHERE ID =" + textBox7.Text);
+			SqlCommand cmd3 = new SqlCommand("UPDATE Memor SET Местоположение = " + textBox1.Text + "WHERE ID =" + textBox7.Text);
+			SqlCommand cmd4 = new SqlCommand("UPDATE Memor SET Описание = " + textBox1.Text + "WHERE ID =" + textBox7.Text);
+            cmd.Connection = Con;
+			cmd1.Connection = Con;
+			cmd2.Connection = Con;
+			cmd3.Connection = Con;
+			cmd4.Connection = Con;
+			cmd.ExecuteNonQuery();
+			cmd1.ExecuteNonQuery();
+			cmd2.ExecuteNonQuery();
+			cmd3.ExecuteNonQuery();
+			cmd4.ExecuteNonQuery();
+			SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Memor", Con);
+			DataSet ds = new DataSet();
+			adapter.Fill(ds);
+			dataGridView1.DataSource = ds.Tables[0];
+		}
+
+		private void label8_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
 }
